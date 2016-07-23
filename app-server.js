b044bc0547f14dbbe63c935aef22bd9f8a1144b2
Stone <sustone@administorsmbp3.attlocal.net>
2016-07-23 11:33:35 -0400

    added socket.io

--- app-server.js
+++ app-server.js
@@ -5,5 +5,14 @@ var app = express();
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(3000);
-console.log("Polling server is running at 'http://localhost:3000'");
+var server = app.listen(3001);
+
+//create a socket server that is also listening on port 3001
+var io = require('socket.io').listen(server);
+
+//event handler for when a socket connects
+io.sockets.on('connection', function(socket) {
+    console.log("Connected: %s", socket.id);
+});
+
+console.log("Polling server is running at 'http://localhost:3001'");
--- components/APP.js
+++ components/APP.js
@@ -1,8 +1,24 @@
+// application entry point
 var React = require('react');
+var io = require('socket.io-client');
+var Header = require('./parts/Header');
 
 var APP = React.createClass({
+	// when component mount we want to setup a socket
+	componentWillMount() {
+		this.socket = io('http://localhost:3001');
+		this.socket.on('connect', this.connect);
+	},
+
+	connect() {
+		alert('connected: ' + this.socket.id);
+	},
 	render() {
-		return (<h1>Hello World form React</h1>);
+		return (
+			<div>
+				<Header title="New Header" />
+			</div>
+		);
 	}
 });
 
